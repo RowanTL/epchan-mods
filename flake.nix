b046d277ff8582589a030b2d6ce63bcc6358b50f
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-03-10 20:23:57 -0500

    flake init

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A flake for developing in a clojure environment with vscode";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { nixpkgs, ... }:
+  let system = "x86_64-linux"; in
+  {
+    devShells."${system}".default =
+    let
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in
+      pkgs.mkShellNoCC {
+        buildInputs = [ pkgs.bashInteractive ];
+        packages = with pkgs; [
+          (python3.withPackages (pypkgs: [
+            pypkgs.pandas
+            pypkgs.polars
+            pypkgs.matplotlib
+            pypkgs.numpy
+          ]))
+          (vscode-with-extensions.override {
+            vscode = vscodium;
+            vscodeExtensions = [
+              ms-python.python
+              ms-azuretools.vscode-docker
+              # pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+              # {
+              #   name = "calva";
+              #   publisher = "betterthantomorrow";
+              #   version = "v2.0.486";
+              #   sha256 = "sha256-pL+OgJvIK5eqE5Kr/wDeJ+2BGUT9Uj42coWSHtbPolk=";
+              # }
+            ];
+          })
+        ];
+        shellHook = ''
+          export SHELL=${pkgs.lib.getExe pkgs.bashInteractive}
+          echo "starting codium"
+          codium .
+        '';
+      };
+  };
+}
